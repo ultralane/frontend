fc4cfb8c2b2bce1bec61dcc000c813eee13b1d98
SauravKanchan <sauravnk30@gmail.com>
2024-03-16 11:06:14 +0000

    fix send

--- src/pages/Send.jsx
+++ src/pages/Send.jsx
@@ -7,64 +7,147 @@ import loadingAnimation from "../assets/loading.json";
 import errorAnimation from "../assets/error.json";
 import { useState } from "react";
 import Loading from "../components/Loading";
+import { Networks } from "../utils/constants";
+import { getTree } from "../utils/blockchain";
+import { db } from "../utils/db";
+import { Note, Field, KeyPair } from "@ultralane/sdk";
+import { useEffect } from "react";
+import { parseUnits } from "ethers";
 
 function Send() {
   const [success, setSuccess] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const sendHandler = () => {
+  const [selectedNetwork, setSelectedNetwork] = useState(""); // Added state for selected network
+  const [amount, setAmount] = useState(10); // Initial amount
+  const [walletAddress, setWalletAddress] = useState("");
+  useEffect(() => {
+    let selectNetwork = async () => {
+      let chainId = parseInt(
+        await window.ethereum.request({ method: "eth_chainId" })
+      ).toString();
+      setSelectedNetwork(chainId);
+    };
+    selectNetwork();
+  }, []);
+
+  const sendHandler = async () => {
     setLoading(true);
-    setTimeout(() => {
+    try {
+      console.log("Sending", amount, "USDC to", walletAddress);
+
+      const tree = await getTree();
+
+      // Get the notes with highest value
+      let notes = await db.getAll("notes");
+      notes = notes.sort((a, b) => {
+        return parseInt(b.amount) - parseInt(a.amount);
+      });
+      let depositNotes = [];
+      for (let note of notes.slice(0, 2)) {
+        let n = await Note.from(note);
+        depositNotes.push(n);
+      }
+
+      const withdrawAmount = parseUnits(amount + "", 6);
+      const withdrawAddress = await Field.from(walletAddress);
+      const keypair = await KeyPair.random();
+      window.depositNotes = depositNotes;
+      const tx = await tree.createTransaction({
+        inputNotes: depositNotes,
+        keypair,
+        updateTree: true,
+        depositAmount: withdrawAmount * -1n,
+        withdrawAddress,
+      });
+      const { proof, publicInputs } = await tx.prove();
+      console.log("Proof and public inputs generated");
       setLoading(false);
       setSuccess(true);
-      // setError(true)
-      setTimeout(() => {
-        setSuccess(false);
-        // setError(false)
-      }, 1400);
-    }, 4000);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+      setError(true);
+    }
+    setTimeout(() => {
+      setSuccess(false);
+      setError(false);
+      setLoading(false);
+    }, 3000);
   };
+
+  // Handler for network selection change
+  const handleNetworkChange = (e) => {
+    setSelectedNetwork(e.target.value);
+  };
+
+  // Handler for amount change
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+
   return (
     <PageWrapper>
       <Sidebar />
-      <div className="mt-20 flex justify-center ">
-        <div className="w-[32%] flex flex-col items-center gap-2">
-          {/* {
-          success &&
-      <div className="fixed inset-0 z-50 overflow-auto flex justify-center items-center bg-slate-900 bg-opacity-50">
-      <Lottie animationData={successAnimation}/>
-</div>
-        } */}
-          {loading && (
-            // <div className="fixed card-gradient-2 inset-0 z-50 overflow-auto flex justify-center items-center bg-slate-900 bg-opacity-50">
-            <Loading animation={loadingAnimation} />
-          )}
-
+      <div className='mt-20 flex justify-center'>
+        <div className='w-[32%] flex flex-col items-center gap-2'>
+          {loading && <Loading animation={loadingAnimation} />}
           {success && <Loading animation={successAnimation} />}
-
           {error && <Loading animation={errorAnimation} />}
-
           {!error && !success && !loading && (
             <>
-              <div className="card-gradient pt-10 pb-4 rounded-2xl w-[100%]">
-                <div className="flex flex-col mb-3 items-center gap-4">
-                  <label className="text-[0.8rem]">You're sending</label>
-                  <h2 className="text-5xl font-bold">95</h2>
-                  <h2 className="text-[#93A0B9] font-medium flex gap-1 items-center ">
-                    95 USD
+              <div className='card-gradient pt-10 pb-4 rounded-2xl w-[100%]'>
+                <div className='flex flex-col mb-3 items-center gap-4'>
+                  <label className='text-[0.8rem]'>You're sending</label>
+                  <input
+                    style={{
+                      WebkitAppearance: "none",
+                      MozAppearance: "textfield",
+                      appearance: "none",
+                      textAlign: "center",
+                      fontSize: "2.5rem", // Adjust the size as needed
+                      fontWeight: "bold",
+                      background: "transparent",
+                      border: "none",
+                      outline: "none",
+                    }}
+                    type='number'
+                    className='text-5xl font-bold bg-transparent text-center border-none outline-none'
+                    value={amount}
+                    onChange={handleAmountChange}
+                  />
+                  <h2 className='text-[#93A0B9] font-medium flex gap-1 items-center'>
+                    USDC
                     <RiArrowUpDownFill />
                   </h2>
                 </div>
-                <div className="p-[.71px] bg-black"></div>
-                <div className="flex justify-between px-6 pt-2 pb-2">
-                  <label htmlFor="" className="text-[.8rem] font-medium">
-                    Select Token
-                  </label>
-                  <select className="text-white bg-inherit border-none"></select>
+                <div className='p-[.71px] bg-black'></div>
+                <div className='flex justify-between px-6 pt-2 pb-2'>
+                  <select
+                    id='network-select'
+                    className='text-white bg-inherit text-center border-none w-full px-2 py-1 appearance-none'
+                    value={selectedNetwork}
+                    onChange={handleNetworkChange}
+                    style={{ display: "block" }}
+                  >
+                    <option value=''>Select a network</option>
+                    {Object.keys(Networks).map((network) => (
+                      <option key={network} value={network}>
+                        {Networks[network].name}
+                      </option>
+                    ))}
+                  </select>
                 </div>
               </div>
-              <div className="card-gradient p-4 text-[.85rem] font-medium w-full text-center rounded-2xl">
-                Wallet address ENS
+              <div className='card-gradient p-4 text-[.85rem] font-medium w-full text-center rounded-2xl'>
+                <input
+                  type='text'
+                  value={walletAddress}
+                  onChange={(e) => setWalletAddress(e.target.value)}
+                  className='text-white bg-inherit text-center border-none w-full px-2 py-1 appearance-none mt-2'
+                  placeholder='Enter Wallet Address'
+                  style={{ background: "", outline: "none" }}
+                />
               </div>
               <Button
                 disabled={loading}
